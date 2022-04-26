dc49e6d432576ab9cb145f710771b113c6c19f64
Ehsan Hejazi <ehsan.hejazi@outlook.com>
2022-04-26 18:01:52 +0430

    save changes

---
+++ platforms/android-native/build/generated/source/buildConfig/debug/io/liteglue/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package io.liteglue;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "io.liteglue";
+  public static final String BUILD_TYPE = "debug";
+}
